06f6341d7f960d3730899f144aa76df159ac1a4c
vaibhav <60617102+vai4411@users.noreply.github.com>
2020-04-24 19:58:59 +0530

    ADDED: TC-1.1 Sad Message In Constructor Return Sad

--- src/test/java/com/bl/demo/MoodAnalyserTest.java
+++ src/test/java/com/bl/demo/MoodAnalyserTest.java
@@ -8,13 +8,19 @@ public class MoodAnalyserTest {
 
     @Test
     public void givenMood_WhenSadMessage_ShouldReturnSad() {
-        analyser = new MoodAnalyser(mood);
+        analyser = new MoodAnalyser();
         Assert.assertEquals("Sad",analyser.analyseMood("I Am In Sad Mood"));
     }
 
     @Test
     public void givenMood_WhenAnyMessage_ShouldReturnHappy() {
-        analyser = new MoodAnalyser(mood);
+        analyser = new MoodAnalyser();
         Assert.assertEquals("Happy",analyser.analyseMood("I Am In Any Mood"));
+    }
+
+    @Test
+    public void givenMood_WhenSadMessageConstructor_ShouldReturnSad() {
+        analyser = new MoodAnalyser("I Am In Sad Mood");
+        Assert.assertEquals("Sad",analyser.analyseMood());
     }
 }
